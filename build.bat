597ba619f99e18fea44f88131200bcca2b3086e6
M4RCK5 <user.andresmarcos@gmail.com>
2026-09-07 05:46:35 -0300

    Add files via upload

--- build.bat
+++ build.bat
@@ -1,2 +1,12 @@
 @echo off
-cl /LD /O2 /EHsc src/main.c /link /DLL /OUT:EOSSDK-Win64-Shipping.dll User32.lib
+setlocal
+
+cl /LD /O2 "%~dp0src\main.c" /link /OUT:"%~dp0EOSSDK-Win64-Shipping.dll" User32.lib
+
+if errorlevel 1 (
+    echo Build failed.
+    exit /b 1
+)
+
+echo Build successful.
+exit /b 0
